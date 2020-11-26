942ee8314edb5ac3d0856ca69124729f9580f989
shimanakahibiki <75057530+shimanakahibiki@users.noreply.github.com>
2020-11-26 16:05:38 +0900

    日記を追加しました。

---
+++ diaries/k19053-diary.py
@@ -0,0 +1,13 @@
+from diaries.AbstractDiary import AbstractDiary
+
+
+class DiarySample(AbstractDiary):
+
+    def get_date(self):
+        return "2020-11-25"
+
+    def get_summary(self):
+        return "一限から眠かった"
+
+    def get_author(self):
+        return "島中"
